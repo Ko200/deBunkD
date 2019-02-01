c36d338ac701105c42635c7749bcce03396b5710
dravog7 <dravog77@gmail.com>
2019-02-01 12:11:30 +0530

    pwaify

--- sw.js
+++ sw.js
@@ -14,3 +14,46 @@ self.addEventListener('install', function(e) {
       })
     );
   });
+
+  self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+      caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== cacheName && key !== dataCacheName) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+    );
+    /*
+     * Fixes a corner case in which the app wasn't returning the latest data.
+     * You can reproduce the corner case by commenting out the line below and
+     * then doing the following steps: 1) load app for first time so that the
+     * initial New York City data is shown 2) press the refresh button on the
+     * app 3) go offline 4) reload the app. You expect to see the newer NYC
+     * data, but you actually see the initial data. This happens because the
+     * service worker is not yet activated. The code below essentially lets
+     * you activate the service worker faster.
+     */
+    return self.clients.claim();
+  });
+
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+  caches.match(event.request).then(function(response) {
+    if (response) {
+      return response;
+    } else {
+      return fetch(event.request).then(function(res) {
+        return caches.open(cacheName).then(function(cache) {
+          cache.put(event.request.url, res.clone());
+          return res;
+        });
+      });
+    }
+  })
+    );
+  });
+  
