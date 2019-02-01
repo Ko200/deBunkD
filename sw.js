76c457d3a3de6e9f14f9df9df8887d372a30c8bd
dravog7 <dravog77@gmail.com>
2019-02-01 18:27:26 +0530

    calender traverse

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 var cacheName = 'deBunkD';
 var filesToCache = [
     'main.css',
-    'index.html',
-    'calendar.html',
-    'app.js',
+    //'index.html',
+    //'calendar.html',
+    //'app.js',
     'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
     'https://code.jquery.com/jquery-3.3.1.min.js',
     'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
