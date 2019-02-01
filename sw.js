e9a1b54aa793dfa4d6ac703c51473ed8d4c0ed20
dravog7 <dravog77@gmail.com>
2019-02-01 11:42:58 +0530

    pwaify

---
+++ sw.js
@@ -0,0 +1,16 @@
+
+var filesToCache = [
+    '/main.css',
+    '/index.html',
+    '/app.js',
+  ];
+
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+      caches.open(cacheName).then(function(cache) {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(filesToCache);
+      })
+    );
+  });
