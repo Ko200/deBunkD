425419a7bd043d7d40d044d3534426841de6c67e
dravog7 <dravog77@gmail.com>
2019-02-01 14:28:38 +0530

    cal

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@ var cacheName = 'deBunkD';
 var filesToCache = [
     'main.css',
     'index.html',
+    'calendar.html',
     'app.js',
     'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
     'https://code.jquery.com/jquery-3.3.1.min.js',
